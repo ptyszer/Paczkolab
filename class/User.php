d8e88996cfb2932d93e7d816e9bd7a0b646eb33a
ptyszer <p.tyszer@hotmail.com>
2018-03-10 15:20:27 +0100

    classes: Parcel, Size, User

--- class/User.php
+++ class/User.php
@@ -2,5 +2,112 @@
 
 class User implements Action
 {
+    private $id;
+    private $name;
+    private $surname;
+    private $credits;
+    private $address;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param mixed $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCredits()
+    {
+        return $this->credits;
+    }
+
+    /**
+     * @param mixed $credits
+     */
+    public function setCredits($credits)
+    {
+        $this->credits = $credits;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete()
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public static function load($id = null)
+    {
+        // TODO: Implement load() method.
+    }
+
+    public static function loadAll()
+    {
+        // TODO: Implement loadAll() method.
+    }
+
+    public static function setDb(Database $db)
+    {
+        // TODO: Implement setDb() method.
+    }
 }
