4b18a4c3d1c498b275ebfba0afac1516849a8a01
ptyszer <p.tyszer@hotmail.com>
2018-04-22 00:16:50 +0200

    Fix Size class

--- class/Size.php
+++ class/Size.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once __DIR__.'/interface/Action.php';
+
 class Size implements Action
 {
     private $id;
@@ -35,8 +38,14 @@ class Size implements Action
         self::$db->query("INSERT INTO Size SET size=:size, price=:price");
         self::$db->bind(':size', $this->getSize(), PDO::PARAM_STR);
         self::$db->bind(':price', $this->getPrice(), PDO::PARAM_STR);
+
         self::$db->execute();
-        return $this;
+
+        if (self::$db->rowCount() >= 1) {
+            return new Size(self::$db->lastInsertId(), $this->getSize(), $this->getPrice());
+        }
+
+        return null;
     }
     public function update()
     {
@@ -44,27 +53,48 @@ class Size implements Action
         self::$db->bind(':id', $this->getId(), PDO::PARAM_INT);
         self::$db->bind(':size', $this->getSize(), PDO::PARAM_STR);
         self::$db->bind(':price', $this->getPrice(), PDO::PARAM_STR);
+
         self::$db->execute();
-        return $this;
+
+        if (self::$db->rowCount() >= 1) {
+            return $this;
+        }
+
+        return null;
     }
     public function delete()
     {
         self::$db->query("DELETE FROM Size WHERE id=:id");
         self::$db->bind(':id', $this->getId(), PDO::PARAM_INT);
+
         self::$db->execute();
-        return $this;
+
+        if (self::$db->rowCount() >= 1) {
+            return true;
+        }
+
+        return false;
     }
     public static function load($id = null)
     {
         self::$db->query("SELECT * FROM Size WHERE id=:id");
         self::$db->bind(':id', $id, PDO::PARAM_INT);
         $row = self::$db->single();
-        return new Size($row['id'], $row['size'], $row['price']);
+
+        if (self::$db->rowCount() >= 1) {
+            return new Size($row['id'], $row['size'], $row['price']);
+        }
+        return null;
     }
     public static function loadAll()
     {
         self::$db->query("SELECT * FROM Size");
-        return self::$db->resultSet();
+        $result = self::$db->resultSet();
+
+        if ($result) {
+            return $result;
+        }
+        return null;
     }
     public static function setDb(Database $db)
     {
