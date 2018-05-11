2b9ac2a7507ada9d898a382df01a9642ab1f8aef
ptyszer <p.tyszer@hotmail.com>
2018-05-11 16:28:46 +0200

    Add user credits check feature

--- restEndPoints/Parcel.php
+++ restEndPoints/Parcel.php
@@ -1,19 +1,32 @@
 <?php
 Parcel::setDb($db);
+User::setDb($db);
+Size::setDb($db);
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $parcels = Parcel::loadAll();
     $tmpParcels = [];
     foreach ($parcels as $k => $parcel) {
         $tmpParcels[$k]['id'] = $parcel['id'];
-        $tmpParcels[$k]['sender_id'] = $parcel['sender_id'];
+        $tmpParcels[$k]['user_id'] = $parcel['user_id'];
         $tmpParcels[$k]['size_id'] = $parcel['size_id'];
         $tmpParcels[$k]['address_id'] = $parcel['address_id'];
     }
     $response = $tmpParcels;
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $parcel = new Parcel(null, $_POST['user_id'], $_POST['size_id'], $_POST['address_id']);
-    $parcel->save();
-    $response = ['success' => [json_decode(json_encode($parcel), true)]];
+    $userId = $_POST['user_id'];
+    $sizeId = $_POST['size_id'];
+    $addressId = $_POST['address_id'];
+    $user = User::load($userId);
+    $size = Size::load($sizeId);
+
+    if($user->getCredits() >= $size->getPrice()){
+        $credits = $user->getCredits() - $size->getPrice();
+        $user->setCredits($credits);
+        $parcel = new Parcel(null, $userId, $sizeId, $addressId);
+        $parcel->save();
+        $user->update();
+        $response = ['success' => [json_decode(json_encode($parcel), true)]];
+    }
 } elseif ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
     parse_str(file_get_contents("php://input"), $patchVars);
     $parcel = Parcel::load($patchVars['id']);
